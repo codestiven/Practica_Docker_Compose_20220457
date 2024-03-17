b6c4e63e07f772bd53fa77e6173e4f7f32d5e65a
Stiven <codestiven@gmail.com>
2024-03-17 11:49:57 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+# Dockerfile
+FROM php:7.4-apache
+
+COPY src/ /var/www/html/
+
+RUN docker-php-ext-install mysqli
---
+++ src/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+                        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+            font-weight:900;
+        }
+
+        .nombre {
+            font-size: 100px;
+            margin-top: 100px;
+            color: brown;
+
+        }
+
+        .numero {
+            font-size: 50px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="nombre">STIVEN DE LA ROSA BRITO</div>
+    <div class="numero">20220457</div>
+</body>
+</html>
